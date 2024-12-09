9bc33fef5a5419a2bf445083d3a57f18d2025e43
Josh Чернов <961877+joshchernoff@users.noreply.github.com>
2024-12-08 19:09:40 -0800

    refactor: clean up logic

--- lib/event_cal_web/live/index.ex
+++ lib/event_cal_web/live/index.ex
@@ -3,120 +3,78 @@ defmodule EventCalWeb.Live.Index do
 
   def mount(_params, _session, socket) do
     now = Timex.now("UTC")
-    next_month = Timex.shift(now, months: 1)
-
-    current_cal = generate_calendar_days(now)
-    next_cal = generate_calendar_days(next_month)
-
-    {:ok,
-     socket
-     |> assign(
-       timezone: nil,
-       current_month: now |> Timex.format!("{Mfull}"),
-       next_month: next_month |> Timex.format!("{Mfull}"),
-       current_month_year: Timex.format!(now, "{YYYY}"),
-       next_month_year: Timex.format!(next_month, "{YYYY}"),
-       now: now |> Timex.format!("{0M}/{0D}/{YY} {h12}:{m} {AM}"),
-       current_cal: current_cal,
-       next_cal: next_cal
-     )}
+    {:ok, assign_month_data(socket, now, nil, Timex.beginning_of_month(now))}
   end
 
   def handle_event("set_timezone", %{"timezone" => timezone}, socket) do
     now = Timex.now(timezone)
-    next_month = Timex.shift(now, months: 1)
+    {:noreply, assign_month_data(socket, now, timezone, Timex.beginning_of_month(now))}
+  end
 
-    current_cal = generate_calendar_days(now)
-    next_cal = generate_calendar_days(next_month)
+  def handle_event("prev-month", _params, socket) do
+    shift_month(socket, -1)
+  end
+
+  def handle_event("next-month", _params, socket) do
+    shift_month(socket, 1)
+  end
+
+  def shift_month(socket, direction) do
+    current_month = parse_month(socket.assigns.current_month, socket.assigns.current_month_year)
+    updated_month = Timex.shift(current_month, months: direction)
 
     {:noreply,
-     assign(socket,
-       timezone: timezone,
-       current_month: now |> Timex.format!("{Mfull}"),
-       next_month: next_month |> Timex.format!("{Mfull}"),
-       current_month_year: Timex.format!(now, "{YYYY}"),
-       next_month_year: Timex.format!(next_month, "{YYYY}"),
-       now: now |> Timex.format!("{0M}/{0D}/{YY} {h12}:{m} {AM}"),
-       current_cal: current_cal,
-       next_cal: next_cal
+     assign_month_data(
+       socket,
+       Timex.now(socket.assigns.timezone),
+       socket.assigns.timezone,
+       updated_month
      )}
   end
 
-  def handle_event("prev-month", _unsigned_params, socket) do
-    now = Timex.now(socket.assigns.timezone)
+  defp assign_month_data(socket, now, timezone, current_month) do
+    next_month = Timex.shift(current_month, months: 1)
 
-    current_month =
-      Timex.parse!(
-        "#{socket.assigns.current_month} 1, #{socket.assigns.current_month_year}",
-        "{Mfull} {D}, {YYYY}"
-      )
-      |> Timex.shift(months: -1)
-
-    next_month = current_month |> Timex.shift(months: 1)
-
-    current_cal = generate_calendar_days(current_month)
-    next_cal = generate_calendar_days(next_month)
-
-    {:noreply,
-     assign(socket,
-       current_month: current_month |> Timex.format!("{Mfull}"),
-       next_month: socket.assigns.current_month,
-       current_month_year: Timex.format!(current_month, "{YYYY}"),
-       next_month_year: socket.assigns.current_month_year,
-       now: now |> Timex.format!("{0M}/{0D}/{YY} {h12}:{m} {AM}"),
-       current_cal: current_cal,
-       next_cal: next_cal
-     )}
+    assign(socket,
+      timezone: timezone,
+      now: Timex.format!(now, "{0M}/{0D}/{YY} {h12}:{m} {AM}"),
+      current_month: Timex.format!(current_month, "{Mfull}"),
+      next_month: Timex.format!(next_month, "{Mfull}"),
+      current_month_year: Timex.format!(current_month, "{YYYY}"),
+      next_month_year: Timex.format!(next_month, "{YYYY}"),
+      current_cal: generate_calendar_days(current_month),
+      next_cal: generate_calendar_days(next_month)
+    )
   end
 
-  def handle_event("next-month", _unsigned_params, socket) do
-    now = Timex.now(socket.assigns.timezone)
-
-    next_month =
-      Timex.parse!(
-        "#{socket.assigns.next_month} 1, #{socket.assigns.next_month_year}",
-        "{Mfull} {D}, {YYYY}"
-      )
-      |> Timex.shift(months: 1)
-
-    next_cal = generate_calendar_days(next_month)
-    current_cal = next_month |> Timex.shift(months: -1) |> generate_calendar_days()
-
-    {:noreply,
-     assign(socket,
-       current_month: socket.assigns.next_month,
-       current_month_year: socket.assigns.next_month_year,
-       next_month: next_month |> Timex.format!("{Mfull}"),
-       next_month_year: Timex.format!(next_month, "{YYYY}"),
-       now: now |> Timex.format!("{0M}/{0D}/{YY} {h12}:{m} {AM}"),
-       current_cal: current_cal,
-       next_cal: next_cal
-     )}
+  defp parse_month(month, year) do
+    Timex.parse!("#{month} 1, #{year}", "{Mfull} {D}, {YYYY}")
   end
 
   defp generate_calendar_days(date) do
     first_date = Timex.beginning_of_month(date)
-
-    start_date =
-      date
-      |> Timex.beginning_of_month()
-      |> Date.add(-(Timex.weekday(first_date) |> rem(7)))
-
     last_date = Timex.end_of_month(date)
 
-    end_date =
-      date
-      |> Timex.end_of_month()
-      |> Date.add(6 - rem(Date.day_of_week(last_date), 7))
+    start_date = adjust_start_date(first_date)
+    end_date = adjust_end_date(last_date)
 
-    days = Enum.to_list(Date.range(start_date, end_date))
+    Date.range(start_date, end_date)
+    |> Enum.map(&classify_day(&1, first_date, last_date))
+  end
 
-    Enum.map(days, fn day ->
-      if Date.compare(day, first_date) in [:lt] or Date.compare(day, last_date) in [:gt] do
-        {:outside, day.day}
-      else
-        {:inside, day.day}
-      end
-    end)
+  defp adjust_start_date(first_date) do
+    first_date |> Date.add(-Timex.weekday(first_date) |> rem(7))
+  end
+
+  defp adjust_end_date(last_date) do
+    last_date |> Date.add(6 - rem(Date.day_of_week(last_date), 7))
+  end
+
+  defp classify_day(day, first_date, last_date) do
+    if Date.compare(day, first_date) == :lt or Date.compare(day, last_date) == :gt do
+      {:outside, day.day}
+    else
+      {:inside, day.day}
+    end
   end
 end
